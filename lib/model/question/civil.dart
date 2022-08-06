9a0233ab805f053a3509577936da917f93a18968
Manjil Koirala <koiralamanjil7@gmail.com>
2022-08-06 12:54:18 +0545

    today

--- lib/model/question/civil.dart
+++ lib/model/question/civil.dart
@@ -2,13 +2,37 @@ import 'package:punotes/model/notes/notemodel.dart';
 
 class question {
   static final civil = [
-    Notes(subjecttitle: 'First Semester', path: ''),
-    Notes(subjecttitle: 'Second Semester', path: ''),
-    Notes(subjecttitle: 'Third Semester', path: ''),
-    Notes(subjecttitle: 'Fourth Semester', path: ''),
-    Notes(subjecttitle: 'Fifth Semester', path: ''),
-    Notes(subjecttitle: 'Sixth Semester', path: ''),
-    Notes(subjecttitle: 'Seventh Semester', path: ''),
-    Notes(subjecttitle: 'Eight Semester', path: ''),
+    Notes(
+        subjecttitle: 'First Semester',
+        path:
+            'https://firebasestorage.googleapis.com/v0/b/mynotes-flutter-my.appspot.com/o/civil_QB%2F1st%20semester.pdf?alt=media&token=0030a7b9-3f05-44e1-b283-e08a6b936abf'),
+    Notes(
+        subjecttitle: 'Second Semester',
+        path:
+            'https://firebasestorage.googleapis.com/v0/b/mynotes-flutter-my.appspot.com/o/civil_QB%2F2nd%20sem%20civil%20qb.pdf?alt=media&token=6d8a50e1-b156-4610-876f-5f3da4d89cbe'),
+    Notes(
+        subjecttitle: 'Third Semester',
+        path:
+            'https://firebasestorage.googleapis.com/v0/b/mynotes-flutter-my.appspot.com/o/civil_QB%2F3rd%20semester.pdf?alt=media&token=7d98163d-439c-4a87-8e09-1c6842ced01c'),
+    Notes(
+        subjecttitle: 'Fourth Semester',
+        path:
+            'https://firebasestorage.googleapis.com/v0/b/mynotes-flutter-my.appspot.com/o/civil_QB%2F4TH%20SEMESTER.pdf?alt=media&token=a40bed71-78c7-4b1e-b638-f32d1b0184b0'),
+    Notes(
+        subjecttitle: 'Fifth Semester',
+        path:
+            'https://firebasestorage.googleapis.com/v0/b/mynotes-flutter-my.appspot.com/o/civil_QB%2F5TH%20SEMESTER.pdf?alt=media&token=6b7c9378-34ac-4ae2-b3bb-5af4a55fff26'),
+    Notes(
+        subjecttitle: 'Sixth Semester',
+        path:
+            'https://firebasestorage.googleapis.com/v0/b/mynotes-flutter-my.appspot.com/o/civil_QB%2F6TH%20SEMESTER.pdf?alt=media&token=5c4ecaff-7fc8-4ade-860b-85dd8d8fe58d'),
+    Notes(
+        subjecttitle: 'Seventh Semester',
+        path:
+            'https://firebasestorage.googleapis.com/v0/b/mynotes-flutter-my.appspot.com/o/civil_QB%2F7TH%20SEMESTER.pdf?alt=media&token=61af0e3b-d6e6-4d94-b4ba-d25a6b4ffd3f'),
+    Notes(
+        subjecttitle: 'Eight Semester',
+        path:
+            'https://firebasestorage.googleapis.com/v0/b/mynotes-flutter-my.appspot.com/o/civil_QB%2F8th%20SEM.pdf?alt=media&token=da9ecb9a-8648-4f0a-b7b7-12a87cac507d'),
   ];
 }

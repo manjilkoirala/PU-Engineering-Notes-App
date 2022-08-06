4e5ff0044bf3e53670c3a83294d74bdb214846fb
Manjil Koirala <koiralamanjil7@gmail.com>
2022-08-06 13:39:52 +0545

    model

---
+++ lib/model/questions.dart
@@ -0,0 +1,84 @@
+import 'package:punotes/model/notes/notemodel.dart';
+
+class Question {
+  static final civil = [
+    Notes(
+        subjecttitle: 'First Semester Civil',
+        path:
+            'https://firebasestorage.googleapis.com/v0/b/mynotes-flutter-my.appspot.com/o/civil_QB%2F1st%20semester.pdf?alt=media&token=0030a7b9-3f05-44e1-b283-e08a6b936abf'),
+    Notes(
+        subjecttitle: 'Second Semester Civil',
+        path:
+            'https://firebasestorage.googleapis.com/v0/b/mynotes-flutter-my.appspot.com/o/civil_QB%2F2nd%20sem%20civil%20qb.pdf?alt=media&token=6d8a50e1-b156-4610-876f-5f3da4d89cbe'),
+    Notes(
+        subjecttitle: 'Third Semester Civil',
+        path:
+            'https://firebasestorage.googleapis.com/v0/b/mynotes-flutter-my.appspot.com/o/civil_QB%2F3rd%20semester.pdf?alt=media&token=7d98163d-439c-4a87-8e09-1c6842ced01c'),
+    Notes(
+        subjecttitle: 'Fourth Semester Civil',
+        path:
+            'https://firebasestorage.googleapis.com/v0/b/mynotes-flutter-my.appspot.com/o/civil_QB%2F4TH%20SEMESTER.pdf?alt=media&token=a40bed71-78c7-4b1e-b638-f32d1b0184b0'),
+    Notes(
+        subjecttitle: 'Fifth Semester Civil',
+        path:
+            'https://firebasestorage.googleapis.com/v0/b/mynotes-flutter-my.appspot.com/o/civil_QB%2F5TH%20SEMESTER.pdf?alt=media&token=6b7c9378-34ac-4ae2-b3bb-5af4a55fff26'),
+    Notes(
+        subjecttitle: 'Sixth Semester Civil',
+        path:
+            'https://firebasestorage.googleapis.com/v0/b/mynotes-flutter-my.appspot.com/o/civil_QB%2F6TH%20SEMESTER.pdf?alt=media&token=5c4ecaff-7fc8-4ade-860b-85dd8d8fe58d'),
+    Notes(
+        subjecttitle: 'Seventh Semester',
+        path:
+            'https://firebasestorage.googleapis.com/v0/b/mynotes-flutter-my.appspot.com/o/civil_QB%2F7TH%20SEMESTER.pdf?alt=media&token=61af0e3b-d6e6-4d94-b4ba-d25a6b4ffd3f'),
+    Notes(
+        subjecttitle: 'Eight Semester Civil',
+        path:
+            'https://firebasestorage.googleapis.com/v0/b/mynotes-flutter-my.appspot.com/o/civil_QB%2F8th%20SEM.pdf?alt=media&token=da9ecb9a-8648-4f0a-b7b7-12a87cac507d'),
+  ];
+
+  //Computer
+
+  static final computer = [
+    Notes(subjecttitle: 'First Semester Computer', path: ''),
+    Notes(
+        subjecttitle: 'Second Semester Computer',
+        path:
+            'https://firebasestorage.googleapis.com/v0/b/mynotes-flutter-my.appspot.com/o/Computer_QB%2F2nd%20semester.pdf?alt=media&token=4e47110d-6c4c-42d6-bea9-8dbdd57e438c'),
+    Notes(
+        subjecttitle: 'Third Semester Computer',
+        path:
+            'https://firebasestorage.googleapis.com/v0/b/mynotes-flutter-my.appspot.com/o/Computer_QB%2F3rd%20Semester.pdf?alt=media&token=873c184d-6aa1-4e3b-bc01-54f4da61e751'),
+    Notes(
+        subjecttitle: 'Fourth Semester Computer',
+        path:
+            'https://firebasestorage.googleapis.com/v0/b/mynotes-flutter-my.appspot.com/o/Computer_QB%2F4th%20semester.pdf?alt=media&token=b0d3b4dd-2266-4f7b-96e9-2781d732910b'),
+    Notes(
+        subjecttitle: 'Fifth Semester Computer',
+        path:
+            'https://firebasestorage.googleapis.com/v0/b/mynotes-flutter-my.appspot.com/o/Computer_QB%2F5th%20semester.pdf?alt=media&token=03226c49-3d2e-4842-b3ad-b9bb521acd25'),
+    Notes(subjecttitle: 'Sixth Semester Computer', path: ''),
+    Notes(subjecttitle: 'Seventh Semester Computer', path: ''),
+    Notes(
+        subjecttitle: 'Eight Semester Computer',
+        path:
+            'https://firebasestorage.googleapis.com/v0/b/mynotes-flutter-my.appspot.com/o/Computer_QB%2F8th%20semester.pdf?alt=media&token=1a2c440b-5701-4d12-83dd-650a8e0a6237'),
+  ];
+
+  //electrical
+  static final electrical = [
+    Notes(subjecttitle: 'First Semester Electrical', path: ''),
+    Notes(subjecttitle: 'Second Semester Electrical', path: ''),
+    Notes(
+        subjecttitle: 'Third Semester Electrical',
+        path:
+            'https://firebasestorage.googleapis.com/v0/b/mynotes-flutter-my.appspot.com/o/Electrical_QB%2F3rd%20electrical.pdf?alt=media&token=bb30efc5-8b97-4fc7-8510-bb8f1394bb07'),
+    Notes(
+        subjecttitle: 'Fourth Semester Computer',
+        path:
+            'https://firebasestorage.googleapis.com/v0/b/mynotes-flutter-my.appspot.com/o/Electrical_QB%2F4th%20electrical.pdf?alt=media&token=8be305f1-6a53-4281-8cf8-0c7df1919ee6'),
+    Notes(subjecttitle: 'Fifth Semester Computer', path: ''),
+    Notes(subjecttitle: 'Sixth Semester Computer', path: ''),
+    Notes(subjecttitle: 'Seventh Semester Computer', path: ''),
+    Notes(subjecttitle: 'Eight Semester Computer', path: ''),
+  ];
+}

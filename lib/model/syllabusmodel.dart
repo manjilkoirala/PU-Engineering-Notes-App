c19ed7c032351dbc91fc45264b07a52eb5aaed5a
Manjil Koirala <koiralamanjil7@gmail.com>
2022-08-06 21:39:10 +0545

    size decrease

---
+++ lib/model/syllabusmodel.dart
@@ -0,0 +1,83 @@
+import 'package:punotes/model/notes/notemodel.dart';
+
+class Syllabus {
+  static final civil = [
+    Notes(subjecttitle: 'First Semester Civil', path: ''),
+    Notes(subjecttitle: 'Second Semester Civil', path: ''),
+    Notes(
+        subjecttitle: 'Third Semester Civil',
+        path: 'assest/files/syllabus/civil/3civil.pdf'),
+    Notes(
+        subjecttitle: 'Fourth Semester Civil',
+        path: 'assest/files/syllabus/civil/4civil.pdf'),
+    Notes(
+        subjecttitle: 'Fifth Semester Civil',
+        path: 'assest/files/syllabus/civil/5civil.pdf'),
+    Notes(
+        subjecttitle: 'Sixth Semester Civil',
+        path: 'assest/files/syllabus/civil/6civil.pdf'),
+    Notes(
+        subjecttitle: 'Seventh Semester',
+        path: 'assest/files/syllabus/civil/7civil.pdf'),
+    Notes(
+        subjecttitle: 'Eight Semester Civil',
+        path: 'assest/files/syllabus/civil/8civil.pdf'),
+  ];
+
+  //Computer
+
+  static final computer = [
+    Notes(
+        subjecttitle: 'First Semester Computer',
+        path: 'assest/files/syllabus/computer/1syllabus.pdf'),
+    Notes(
+        subjecttitle: 'Second Semester Computer',
+        path: 'assest/files/syllabus/computer/2syllabus.pdf'),
+    Notes(
+        subjecttitle: 'Third Semester Computer',
+        path: 'assest/files/syllabus/computer/3syllabus.pdf'),
+    Notes(
+        subjecttitle: 'Fourth Semester Computer',
+        path: 'assest/files/syllabus/computer/4syllabus.pdf'),
+    Notes(
+        subjecttitle: 'Fifth Semester Computer',
+        path: 'assest/files/syllabus/computer/5syllabus.pdf'),
+    Notes(
+        subjecttitle: 'Sixth Semester Computer',
+        path: 'assest/files/syllabus/computer/6syllabus.pdf'),
+    Notes(
+        subjecttitle: 'Seventh Semester Computer',
+        path: 'assest/files/syllabus/computer/7syllabus.pdf'),
+    Notes(
+        subjecttitle: 'Eight Semester Computer',
+        path: 'assest/files/syllabus/computer/8syllabus.pdf'),
+  ];
+
+  //electrical
+  static final electrical = [
+    Notes(
+        subjecttitle: 'First Semester Electrical',
+        path: 'assest/files/syllabus/computer/1syllabus.pdf'),
+    Notes(
+        subjecttitle: 'Second Semester Electrical',
+        path: 'assest/files/syllabus/computer/2syllabus.pdf'),
+    Notes(
+        subjecttitle: 'Third Semester Electrical',
+        path: 'assest/files/syllabus/electrical/3electrical.pdf'),
+    Notes(
+        subjecttitle: 'Fourth Semester Computer',
+        path: 'assest/files/syllabus/electrical/4electrical.pdf'),
+    Notes(
+        subjecttitle: 'Fifth Semester Computer',
+        path: 'assest/files/syllabus/electrical/5electrical.pdf'),
+    Notes(
+        subjecttitle: 'Sixth Semester Computer',
+        path: 'assest/files/syllabus/electrical/6electrical.pdf'),
+    Notes(
+        subjecttitle: 'Seventh Semester Computer',
+        path: 'assest/files/syllabus/electrical/7electrical.pdf'),
+    Notes(
+        subjecttitle: 'Eight Semester Computer',
+        path: 'assest/files/syllabus/electrical/8electrical.pdf'),
+  ];
+}

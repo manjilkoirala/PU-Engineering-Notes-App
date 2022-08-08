ffcdba78fa24c6972876c75e28ec805771097450
Manjil Koirala <koiralamanjil7@gmail.com>
2022-08-08 22:32:44 +0545

    this one is final

--- lib/Screen/pdfview.dart
+++ lib/Screen/pdfview.dart
@@ -1,109 +1,41 @@
 // ignore_for_file: prefer_typing_uninitialized_variables
 
-import 'package:easy_pdf_viewer/easy_pdf_viewer.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
 import 'package:flutter/material.dart';
 import 'package:punotes/widgets/appbar.dart';
 
-class PDFViewerPage extends StatefulWidget {
+class PDFViewerPage extends StatelessWidget {
   final title;
   final path;
   const PDFViewerPage({Key? key, this.title, this.path}) : super(key: key);
 
   @override
-  State<PDFViewerPage> createState() => _PDFViewerPageState();
-}
-
-class _PDFViewerPageState extends State<PDFViewerPage> {
-  bool _isLoading = true;
-  var document;
-
-  @override
-  void initState() {
-    super.initState();
-    loadDocument();
-  }
-
-  loadDocument() async {
-    document = await PDFDocument.fromAsset(widget.path);
-
-    setState(() => _isLoading = false);
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: MyAppBar(title: widget.title, height: 50.0),
-        body: _isLoading
-            ? Center(
-                child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                // ignore: prefer_const_literals_to_create_immutables
-                children: [
-                  const CupertinoActivityIndicator(),
-                ],
-              ))
-            : PDFViewer(
-                document: document,
-                scrollDirection: Axis.vertical,
-                lazyLoad: false,
-              ));
+        appBar: MyAppBar(title: title, height: 50.0),
+        body: SfPdfViewer.asset(
+          path,
+          onDocumentLoaded: (details) {
+            Text("Loaded");
+          },
+        ));
   }
 }
 
-class PDFViewerPageURL extends StatefulWidget {
+class PDFViewerPageURL extends StatelessWidget {
   final title;
   final link;
   const PDFViewerPageURL({Key? key, this.title, this.link}) : super(key: key);
 
   @override
-  State<PDFViewerPageURL> createState() => _PDFViewerPageURLState();
-}
-
-class _PDFViewerPageURLState extends State<PDFViewerPageURL> {
-  bool _isLoading = true;
-  var pdfDocument;
-
-  @override
-  void initState() {
-    super.initState();
-    loadDocument();
-  }
-
-  loadDocument() async {
-    pdfDocument = await PDFDocument.fromURL(widget.link);
-
-    setState(() {
-      _isLoading = false;
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: MyAppBar(title: widget.title, height: 50.0),
-        body: _isLoading
-            ? Center(
-                child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                // ignore: prefer_const_literals_to_create_immutables
-                children: [
-                  const Padding(
-                    padding: EdgeInsets.all(30.0),
-                    child: LinearProgressIndicator(),
-                  ),
-                  const Text(
-                    "Loding depends upon your internet speed",
-                    style: TextStyle(color: Colors.red, fontSize: 16),
-                  )
-                ],
-              ))
-            : PDFViewer(
-                document: pdfDocument,
-                scrollDirection: Axis.vertical,
-                lazyLoad: false,
-              ));
+        appBar: MyAppBar(title: title, height: 50.0),
+        body: SfPdfViewer.network(
+          link,
+          onDocumentLoaded: (details) {
+            Text("Loaded");
+          },
+        ));
   }
 }
